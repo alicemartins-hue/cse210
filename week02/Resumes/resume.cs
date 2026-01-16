5217caa040187b02ad3b57185d181200f9d10dc8
alicemartins-hue <alicemartins7l@hotmail.com>
2026-01-16 18:43:40 +0000

    exercise1 week02 working with objects, lists, instances with c#

---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _list = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs: ");
+
+        foreach (Job job in _list)
+        {
+            job.Display();
+        }
+        
+    }
+
+}
