0c209a087b5e4b515913b28546b01c6983474a47
alicemartins-hue <alicemartins7l@hotmail.com>
2026-01-21 12:03:42 -0300

    learning about constructors and private methods. week03.

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        
+    }
+
+    public Fraction(int top, int bottom)
+    {
+
+    }
+
+    public GetTop()
+    {
+
+    }
+    
+    
+}
