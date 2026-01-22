9bf94103aac08167d24194a7f36c7b1bb3b57759
alicemartins-hue <alicemartins7l@hotmail.com>
2026-01-21 21:45:36 -0300

    week3 getters and setters fraction exercise. Using constructors and classes on C#

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -6,8 +7,20 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
-        
+        Console.WriteLine("Lets do fraction!");
 
-        
+        Console.Write("Choose a number to be the top: ");
+        int top = int.Parse(Console.ReadLine());
+
+        Console.Write("Choose a number to be the bottom: ");
+        int bottom = int.Parse(Console.ReadLine());
+
+        Fraction fraction1 = new Fraction(top, bottom);
+        string StringFraction = fraction1.GetFractionString();
+        double decimalValue = fraction1.GetDecimalValue();
+
+        Console.WriteLine($"{StringFraction}");
+        Console.WriteLine($"{decimalValue}");
+
     }
 }
