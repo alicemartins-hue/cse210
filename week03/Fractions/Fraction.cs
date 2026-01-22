2e7c26e3b2901de8616d507046be1d6bb9bcaf0c
alicemartins-hue <alicemartins7l@hotmail.com>
2026-01-21 21:23:30 -0300

    Refactor Fraction class: streamline methods and improve formatting

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Dynamic;
 
 public class Fraction
 {
@@ -13,18 +14,46 @@ public class Fraction
 
     public Fraction(int wholeNumber)
     {
-        
+        _top = wholeNumber;
+        _bottom = 1;
     }
 
     public Fraction(int top, int bottom)
     {
-
+        _top = top;
+        _bottom = bottom;
     }
 
-    public GetTop()
+    public int GetTop()
     {
-
+        return _top;
     }
-    
-    
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+
 }
