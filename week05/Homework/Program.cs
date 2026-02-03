e5b5e3d5831de3832d7ef39f725081a8cdbdffcf
Alice Candido <alicecandidomartins@proton.me>
2026-02-03 17:39:55 -0300

    add code to teste assignmet class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment test1 = new Assignment("Luis Carlos","European History");
+        string summary = test1.GetSummary();
+        Console.WriteLine(summary);
+
     }
 }
