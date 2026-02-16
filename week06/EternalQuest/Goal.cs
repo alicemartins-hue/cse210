082ecb78964f0828551cf9c0e9d10e0d5444f001
alicemartins-hue <alicemartins7l@hotmail.com>
2026-02-16 16:46:30 -0300

    Implement ChecklistGoal, EternalGoal, Goal, GoalManager, and SimpleGoal classes with necessary methods and properties

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,27 @@
+using System;
+
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    protected Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public string GetDetailString()
+    {
+
+        return "something";
+    }
+
+    public abstract string GetStringRepresentation();
+}
---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,47 @@
+
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+    
+    public void LoadGoals()
+    {
+        
+    }
+
+}
