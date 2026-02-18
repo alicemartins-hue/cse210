88488a14838196471cf2e2982c59a9f7bf47830d
alicemartins-hue <alicemartins7l@hotmail.com>
2026-02-18 17:44:57 -0300

    Add Cycling and Swimming activities to the ExerciseTracking program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -9,6 +9,8 @@ class Program
         List<Activity> activities = new List<Activity>();
 
         activities.Add(new RunningActivity("03 Nov 2022", 30, 4.8));
+        activities.Add(new CyclingActivity("03 Nov 2022", 30, 20.0));
+        activities.Add(new SwimmingActivity("03 Nov 2022", 30, 40));
 
         foreach (Activity activity in activities)
         {
